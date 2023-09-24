fade6752f57321ab8a84c3fca0e8b9bb0d6ee3f7
SaiHlyanPhyoe7 <saihlyan.phyoe7@gmail.com>
2023-09-24 18:04:27 +0630

    feat:added /{id} route

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,13 +1,17 @@
 <template>
   <div>
-    
+    <p>Products detials for {{ id }}</p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam
+      asperiores qui aut debitis sit voluptates ipsam labore, itaque quibusdam?
+      Veritatis placeat, quaerat labore non molestias rerum qui enim atque
+      possimus!
+    </p>
   </div>
 </template>
 
 <script setup>
-
+const { id } = useRoute().params;
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
